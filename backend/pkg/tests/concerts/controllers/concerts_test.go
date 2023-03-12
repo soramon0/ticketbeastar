32619e16a16c088479c442675592f8de82bade7b
soramon0 <23435312+soramon0@users.noreply.github.com>
2023-03-12 21:10:18 +0100

    test: listing concerts

---
+++ backend/pkg/tests/concerts/controllers/concerts_test.go
@@ -0,0 +1,49 @@
+package controllers_test
+
+import (
+	"testing"
+	"ticketbeastar/pkg/controllers"
+	"ticketbeastar/pkg/models"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func TestConcertsController(t *testing.T) {
+	ts := newTestServer()
+
+	t.Run("List", func(t *testing.T) {
+		ts.setup(t)
+		defer ts.teardown(t)
+		testConcertListing(t, ts)
+	})
+}
+
+func testConcertListing(t *testing.T, ts *testServer) {
+	_, err := time.Parse(time.RFC822, "02 Dec 06 08:00 MST")
+	if err != nil {
+		t.Fatalf("time.Parse() err %v; want nil", err)
+	}
+	concert := models.Concert{
+		Name: "The Red Chord",
+		// Subtitle:            "with Animosity and Lethargy",
+		// Date:                date,
+		// TicketPrice:         3250,
+		// Venue:               "The Mosh Pit",
+		// VenueAddress:        "123 Example Lane",
+		// City:                "Golang city",
+		// State:               "On",
+		// Zip:                 "17916",
+		// AddionalInformation: "For tickets, call (555) 555-5555",
+	}
+	err = ts.cs.Create(&concert)
+	if err != nil {
+		t.Fatalf("Create(concert) err = %v, want nil", err)
+	}
+
+	concertsC := controllers.NewConcerts(ts.cs, ts.log)
+	endpoint := "/api/v1/concerts"
+	ts.app.Get(endpoint, concertsC.GetConcerts)
+
+	ts.hitGetEndpoint(t, endpoint, fiber.StatusOK, 1, "")
+}
---
+++ backend/pkg/tests/concerts/controllers/setup_test.go
@@ -0,0 +1,83 @@
+package controllers_test
+
+import (
+	"context"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http/httptest"
+	"testing"
+	"ticketbeastar/pkg/configs"
+	"ticketbeastar/pkg/database"
+	"ticketbeastar/pkg/models"
+	"ticketbeastar/pkg/utils"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/uptrace/bun"
+)
+
+type testServer struct {
+	app *fiber.App
+	db  *bun.DB
+	cs  models.ConcertService
+	log *log.Logger
+}
+
+func newTestServer() *testServer {
+	logger := utils.InitLogger()
+	app := fiber.New(configs.FiberConfig())
+	db := database.OpenConnection(utils.GetTestDatabaseURL(), logger)
+	cs := models.NewConcertService(db)
+
+	return &testServer{
+		app: app,
+		db:  db,
+		cs:  cs,
+		log: logger,
+	}
+}
+
+func (ts *testServer) setup(t *testing.T) {
+	_, err := ts.db.NewCreateTable().Model((*models.Concert)(nil)).Exec(context.Background())
+	if err != nil {
+		t.Fatalf("NewCreateTable(Concert) err %v; want nil", err)
+	}
+}
+
+func (ts *testServer) teardown(t *testing.T) {
+	defer database.CloseConnection(ts.db)
+
+	_, err := ts.db.NewDropTable().Model((*models.Concert)(nil)).Exec(context.Background())
+	if err != nil {
+		t.Fatalf("Drop concerts table err %v; want nil", err)
+	}
+}
+
+func (ts *testServer) hitGetEndpoint(t *testing.T, endpoint string, wantStatusCode int, wantCount int, wantError string) *models.APIResponse {
+	req := httptest.NewRequest("GET", endpoint, nil)
+	resp, err := ts.app.Test(req)
+	if err != nil {
+		t.Fatalf("GET %s err %v", endpoint, err)
+	}
+	if resp.StatusCode != wantStatusCode {
+		t.Fatalf("statusCode = %d; want %d", resp.StatusCode, wantStatusCode)
+	}
+	body, _ := io.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	var apiResponse models.APIResponse
+	if err := json.Unmarshal(body, &apiResponse); err != nil {
+		t.Fatalf("apiResponse unmarshal() err %v; want nil", err)
+	}
+	if apiResponse.Count != wantCount {
+		log.Fatalf("apiResponse.Count mismatch. want %d; got %d", apiResponse.Count, wantCount)
+	}
+	if wantError == "" && apiResponse.Error != nil {
+		log.Fatalf("apiResponse.Error got %v; want nil", apiResponse.Error)
+	}
+	if apiResponse.Error != nil {
+		if apiResponse.Error.Message != wantError {
+			log.Fatalf("apiResponse.Error got %q; want %q", apiResponse.Error.Message, wantError)
+		}
+	}
+	return &apiResponse
+}
