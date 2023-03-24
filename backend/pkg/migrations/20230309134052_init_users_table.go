6337b5c4df7d2d04ba55cbcb6e36ce39ecd0e528
soramon0 <23435312+soramon0@users.noreply.github.com>
2023-03-24 15:22:31 +0000

    feat: update migration scripts

--- backend/pkg/migrations/20230309134052_init_users_table.go
+++ backend/pkg/migrations/20230309134052_init_users_table.go
@@ -12,11 +12,11 @@ import (
 func init() {
 	Migrations.MustRegister(func(ctx context.Context, db *bun.DB) error {
 		fmt.Print(" [up migration] ")
-		_, err := db.NewCreateTable().Model((*models.User)(nil)).Exec(ctx)
+		_, err := db.NewCreateTable().Model((*models.User)(nil)).IfNotExists().Exec(ctx)
 		return err
 	}, func(ctx context.Context, db *bun.DB) error {
 		fmt.Print(" [down migration] ")
-		_, err := db.NewDropTable().Model((*models.User)(nil)).Exec(ctx)
+		_, err := db.NewDropTable().Model((*models.User)(nil)).IfExists().Exec(ctx)
 		return err
 	})
 }
--- backend/pkg/migrations/20230315163532_init_orders_table.go
+++ backend/pkg/migrations/20230315163532_init_orders_table.go
@@ -12,11 +12,11 @@ import (
 func init() {
 	Migrations.MustRegister(func(ctx context.Context, db *bun.DB) error {
 		fmt.Print(" [up migration] ")
-		_, err := db.NewCreateTable().Model((*models.Order)(nil)).Exec(ctx)
+		_, err := db.NewCreateTable().Model((*models.Order)(nil)).IfNotExists().Exec(ctx)
 		return err
 	}, func(ctx context.Context, db *bun.DB) error {
 		fmt.Print(" [down migration] ")
-		_, err := db.NewDropTable().Model((*models.Order)(nil)).Exec(ctx)
+		_, err := db.NewDropTable().Model((*models.Order)(nil)).IfExists().Exec(ctx)
 		return err
 	})
 }
